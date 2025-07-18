66e93a8a765cbebd4afdf3f42707a8bcbe0c80e6
Jake <75119871+ngljcb@users.noreply.github.com>
2025-07-18 22:44:36 +0200

    added home-header component

---
+++ selfie/src/app/home/home-header/home-header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-header',
+  imports: [],
+  templateUrl: './home-header.component.html',
+  styleUrl: './home-header.component.scss',
+})
+export class HomeHeaderComponent {}
