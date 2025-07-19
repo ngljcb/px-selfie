b53fcb1b15c5a7facc3f8dc387fbb6cc820d6a1f
Jake <75119871+ngljcb@users.noreply.github.com>
2025-07-19 22:29:01 +0200

    added logo + menu on header comp, both mobile & desktop versions

--- selfie/src/app/home/home-header/home-header.component.ts
+++ selfie/src/app/home/home-header/home-header.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home-header.component.html',
   styleUrl: './home-header.component.scss',
 })
-export class HomeHeaderComponent {}
+export class HomeHeaderComponent {
+  menuOpen = false;
+
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
+}
