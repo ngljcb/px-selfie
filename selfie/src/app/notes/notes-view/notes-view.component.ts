989d73f747efe24e38e4862667211705d5e1b673
zanettix <126067706+zanettix@users.noreply.github.com>
2025-07-23 12:48:57 +0200

    creato la componente notes-view dentro a notes

---
+++ selfie/src/app/notes/notes-view/notes-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notes-view',
+  imports: [],
+  templateUrl: './notes-view.component.html',
+  styleUrl: './notes-view.component.scss'
+})
+export class NotesViewComponent {
+
+}
