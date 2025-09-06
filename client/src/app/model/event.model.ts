7741827657a0050b96f3f22beb56af44200ab56e
Jake <75119871+ngljcb@users.noreply.github.com>
2025-09-06 09:53:48 +0200

    update event model

--- client/src/app/model/event.model.ts
+++ client/src/app/model/event.model.ts
@@ -8,7 +8,7 @@ export interface Event {
   start_time: string;       // formato HH:mm:ss
   end_time: string;         // formato HH:mm:ss
   days_recurrence: string;  // es. "Monday,Tuesday"
-  recurrence_type: 'numeroFisso' | 'scadenza' | 'indeterminato';
+  recurrence_type: 'giorniSettimana' | 'numeroFisso' | 'scadenza' | 'indeterminato';
   number_recurrence: number;
   due_date: string;         // formato YYYY-MM-DD
   created_at: string;       // ISO timestamp
