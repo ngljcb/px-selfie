9a071abb0361c4fbb31cecd5658bfaf53df378b8
Jake <75119871+ngljcb@users.noreply.github.com>
2025-09-06 22:03:07 +0200

    version 1.0.0

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 const routes = require('./router');
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL, // client Angular
+  origin: process.env.FRONTEND_URL,
   credentials: true
 }));
 
